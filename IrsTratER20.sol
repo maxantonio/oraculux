f318f82f2528b593b35a297b515e76f58dfc0153
Yoidel <yoidel86@gmail.com>
2018-02-13 17:39:53 -0500

    creando contrato tockens

--- IrsTratER20.sol
+++ IrsTratER20.sol
@@ -5,7 +5,8 @@ contract IrsTratER20 {
     uint256 constant private MAX_UINT256 = 2**256 - 1;
     mapping (address => uint256) public balances;
     mapping (address => mapping (address => uint256)) public permitted;
-   
+
+    uint256 public totalSupply;             //total existente
     string public name;                   //nombre completo ej:moneda Reports IRSTRAT
     uint8 public decimals;                //cantidad decimales a mostrar.
     string public symbol = "IRT";                 //El simbolo del token: eg IRT
@@ -55,5 +56,8 @@ contract IrsTratER20 {
 
     function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
         return permitted[_owner][_spender];
-    }   
+    }
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
