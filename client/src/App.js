480792ed78ed2f0c5dc836e68aa6e260e386c4e0
Yoidel <yoidel86@gmail.com>
2018-02-15 13:14:43 -0500

    agregando grafico tiempo

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,34 @@
 import React, {Component} from 'react';
-import { BarChart,Bar } from 'recharts';
+import { BarChart,Bar,Tooltip } from 'recharts';
 import './App.css';
 
 class App extends Component {
 
     constructor() {
         super();
-        this.uncles = [{block:0,cont:4,fill:"#ccc"}]
+        this.uncles = [{name:0,cont:4,fill:"#ccc"}]
         this.transactions = [
-            {block: 0, uv: 4, fill: "#8884d8"},
-            {block: 0, uv: 3, fill: "#cc3f2c"},
-            {block: 0, uv: 5, fill: "#8884d8"},
-            {block: 0, uv: 2, fill: "#8884d8"},
-            {block: 0, uv: 5, fill: "#8884d8"},
-            {block: 0, uv: 1, fill: "#8884d8"},
-            {block: 0, uv: 1, fill: "#8884d8"},
-            {block: 0, uv: 1, fill: "#8884d8"},
-            {block: 0, uv: 1, fill: "#8884d8"},
-            {block: 0, uv: 1, fill: "#8884d8"},
+            {name: 0, transactions: 4, fill: "#8884d8"},
+            {name: 0, transactions: 3, fill: "#cc3f2c"},
+            {name: 0, transactions: 5, fill: "#8884d8"},
+            {name: 0, transactions: 2, fill: "#8884d8"},
+            {name: 0, transactions: 5, fill: "#8884d8"},
+            {name: 0, transactions: 1, fill: "#8884d8"},
+            {name: 0, transactions: 1, fill: "#8884d8"},
+            {name: 0, transactions: 1, fill: "#8884d8"},
+            {name: 0, transactions: 1, fill: "#8884d8"},
+            {name: 0, transactions: 1, fill: "#8884d8"},
         ];
+
         this.state = {
-            transactions:[{block: 0, uv: 11, fill: "#8884d8"}],
+            transactions:this.transactions,
             last_block: 0,
+            times:[{name:0,time:0,fill:"#ccc"}],
+            avgtime:15,
             best_block: 0,
             lastknow_block:0,
             gas_price:0,
-            uncles:[{block:0,cont:0,fill:"#ccc"}],
+            uncles:[{name:0,cont:0,fill:"#ccc"}],
             uncle_val:0,
             uncle_50:0,
             hash_rate:0,
@@ -56,6 +59,7 @@ class App extends Component {
     }
     setBlock(data){
         if(this.state.best_block !== data) {
+            this.addTime(this.state.last_block,data);
             this.setState({
                 best_block: data,
                 last_block: 0
@@ -96,6 +100,17 @@ class App extends Component {
                 console.log(type+" no esta definido todavia",data)
         }
     }
+    getTimeFill(time){
+        if(time<=10) {
+            return "#5fc46a";
+        }else if(time>10&&time<=20){
+            return "#ffd75a";
+        }else if(time>20&&time<30){
+            return "#ff8812";
+        }else{
+            return "#eb4b4b";
+        }
+    }
     getTransFill(count){
         if(count<5) {
             return "#8884d8"
@@ -105,11 +120,29 @@ class App extends Component {
             return "#008400"
         }
     }
+    addTime(time,block){
 
+        var nuevo = {name: block, cont: time, fill: this.getTimeFill(time)}
+        var temp = [];
+        var total =0;
+        for (var j = 0; j < this.state.times.length; j++) {
+            total+=this.state.times[j].time;
+            temp.push(this.state.times[j]);
+        }
+        var avg = total/temp.length;
+        if( this.state.times.length>49)
+            temp.shift();
+        temp.push(nuevo);
+        this.setState(
+            {
+                times: temp,
+                avgtime:avg
+            });
+    }
     addUncle(count,block){
 
         if(this.uncles.length===0||block!==this.uncles[this.uncles.length-1].block) {
-            var nuevo = {block: block, cont: count, fill: this.getTransFill(count)}
+            var nuevo = {name: block, cont: count, fill: this.getTransFill(count)}
             this.uncles.push(nuevo);
             if(this.uncles.length>50)
                 this.uncles.shift();
@@ -135,7 +168,7 @@ class App extends Component {
             for (var j = 0; j < this.transactions.length; j++) {
                 trans.push(this.transactions[j]);
             }
-            var nuevo = {block: block, uv: count, fill: this.getTransFill(count)}
+            var nuevo = {name: block, transactions: count, fill: this.getTransFill(count)}
             trans.push(nuevo);
             this.transactions = trans
             this.setState({transactions: trans});
@@ -205,7 +238,7 @@ class App extends Component {
                             <div className="pull-left icon tc-yellow"><i className="fa fa-clock-o fa-4x"></i></div>
                             <div className="info">
                                 <span className="title">avg block time</span>
-                                <span className="value tc-yellow">14.79s</span>
+                                <span className="value tc-yellow">{this.state.avgtime}</span>
                             </div>
                         </div>
                     </div>
@@ -271,14 +304,21 @@ class App extends Component {
 
                     <div className="box">
                         <span className="title2">Transactions</span>
-                        <BarChart width={280} height={80} data={this.state.transactions} bind>
-                            <Bar dataKey='uv' fillKey='fill'/>
+                        <BarChart width={280} height={80} data={this.state.transactions} bind >
+                            <Bar dataKey='transactions' fillKey='fill'/>
+                            <Tooltip dataKey='transactions' title='false' />
                         </BarChart>
                     </div>
                     <div className="box">
                         <span className="title2">Uncles</span>
-                        <BarChart width={280} height={80} data={this.state.uncles} bind>
-                            <Bar dataKey='cont' fillKey='fill'/>
+                        <BarChart width={280} height={80}  data={this.state.uncles} bind>
+                            <Bar dataKey='cont' fillKey='fill'  />
+                        </BarChart>
+                    </div>
+                    <div className="box">
+                        <span className="title2">Block Time</span>
+                        <BarChart width={280} height={80}  data={this.state.times} bind>
+                            <Bar dataKey='cont' fillKey='fill'  />
                         </BarChart>
                     </div>
                 </div>
